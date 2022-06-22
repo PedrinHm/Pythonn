a0c33246a66b4f2c1dfac461ece8b0e68ab4a8af
PedrinHm <pedrohm441@gmail.com>
2022-06-22 11:51:29 -0300

    adicao exerc 5

--- codigo5.py
+++ codigo5.py
@@ -1,2 +1,7 @@
-print('teste')
-print('novo teste')
+maior = 0
+
+for i in range(3):
+    num1 = int(input('Digite um número: '))
+    if num1 > maior:
+        maior = num1
+print('o maior número é {}', maior)
